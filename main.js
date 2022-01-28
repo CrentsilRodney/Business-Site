7218011df3fb7d34fbefe1278f96b5c889fc92f3
privoas <40150982+privoas@users.noreply.github.com>
2022-01-28 17:24:17 +0000

    Added a click event to #nav-menu-icon element and added a main.js file.

---
+++ main.js
@@ -0,0 +1,11 @@
+const navIcon = document.querySelector('#nav-menu-icon');
+// You can also use document.getElementById('nav-icon')
+navIcon.addEventListener('click', function(e) {
+  /* e: is the event object. Learn more of JS and use it effectively.
+  * You can also use css to control the visibility of an element here.
+  * Example to control the display of an item with js.
+  * let elementToToggle = document.getElementById('id')
+  * elementToToggle.style.property = value;
+  */
+  alert('Hey clicked'); // replace your code 
+});
